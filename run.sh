1d0b6eae8eb93b73329edc69a8ce0ad2dcd2ee12
joshuadarecullen <joshuadarecullen@protonmail.com>
2025-09-04 11:46:46 +0100

    run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python pdf_to_audiobook.py --pdf_path "/home/joshua/Downloads/KingKill33.pdf" --output_path "/home/joshua/Downloads/KingKill33.mp3" --language "en" --speaker_wav "joshua.wav"
+# python pdf_to_audiobook.py --pdf_path "/home/joshua/Downloads/KingKill33.pdf" --output_path "/home/joshua/Downloads/KingKill33.mp3" --language "en" --speaker_wav "joshua.wav"
+python pdf_to_audiobook.py --pdf_path "/home/joshua/Documents/my_best_man_speech.pdf" --output_path "/home/joshua/Documents/mbms.mp3" --language "en" --speaker_wav "joshua.wav"
